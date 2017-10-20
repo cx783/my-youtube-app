e920d733d60d4af018c339b4564ad1a8b6fdee89
cx783 <cox.miguel1@gmail.com>
2017-10-20 08:52:07 -0500

    Se agrega avances de los componentes de la lista de videos y el iframe

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const KEY = 'AIzaSyA0rZv0C8s5cDfHy4_LvGteaeTh_qiTPPY';
 
@@ -8,16 +10,28 @@ const KEY = 'AIzaSyA0rZv0C8s5cDfHy4_LvGteaeTh_qiTPPY';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { videos: [] };
+    this.state = { 
+      videos: [],
+      selectedVideo: null
+    };
 
     YTSearch({key: KEY, term: 'surfboard'}, (videos) => {
-      this.setState({videos})
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      })
     });
   }
 
   render() {
     return (
-      <SearchBar />
+      <div>
+        <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList 
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
+      </div>
     );
   }
 }
