7a5297bae0cef43704a2867954e2245129bfecc3
cx783 <cox.miguel1@gmail.com>
2017-10-21 09:10:43 -0500

    Se agrega la funcionalidad del buscador

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
+import _ from 'lodash';
+
+import './style.css';
 
 const KEY = 'AIzaSyA0rZv0C8s5cDfHy4_LvGteaeTh_qiTPPY';
 
@@ -10,12 +13,17 @@ const KEY = 'AIzaSyA0rZv0C8s5cDfHy4_LvGteaeTh_qiTPPY';
 class App extends Component {
   constructor(props) {
     super(props);
+
     this.state = { 
       videos: [],
       selectedVideo: null
     };
 
-    YTSearch({key: KEY, term: 'surfboard'}, (videos) => {
+    this.videoSearch('surfboards');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -24,13 +32,17 @@ class App extends Component {
   }
 
   render() {
+    const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
+
     return (
       <div>
-        <SearchBar />
-        <VideoDetail video={this.state.selectedVideo} />
-        <VideoList 
-          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
-          videos={this.state.videos} />
+        <SearchBar onSearchTermChange={videoSearch} />
+        <div className="row">
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList 
+            onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+            videos={this.state.videos} />
+        </div>
       </div>
     );
   }
--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = function () {
-	return (
-		<input type="text" />
-	)
+class SearchBar extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { term: '' };
+	}
+
+	onInputChange(term) {
+		this.setState({ term });
+		this.props.onSearchTermChange(term);
+	}
+
+	render() {	
+		return (
+			<div className="search-bar">
+				<input 
+					type="text" 
+					value={this.state.term}
+					onChange={event => this.onInputChange(event.target.value)}
+					/>
+			</div>
+		);
+	}
 }
 
 export default SearchBar;
